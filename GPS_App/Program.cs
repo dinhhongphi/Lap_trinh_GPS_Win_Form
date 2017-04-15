13ac0584b32a878e671d6c25ca722bbc11d09be3
dinh hong phi <dinhhongphi@hotmail.com>
2017-04-15 09:09:21 +0700

    fix error map don't support browser

--- GPS_App/Program.cs
+++ GPS_App/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,39 @@ namespace GPS_App
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            SetRegistry();
+            Application.EnableVisualStyles();
             Application.Run(new Form1());
+        }
+        static void SetRegistry()
+        {
+            int BrowserVer, RegVal;
+
+            // get the installed IE version
+            using (WebBrowser Wb = new WebBrowser())
+                BrowserVer = Wb.Version.Major;
+
+            // set the appropriate IE version
+            if (BrowserVer >= 11)
+                RegVal = 11001;
+            else if (BrowserVer == 10)
+                RegVal = 10001;
+            else if (BrowserVer == 9)
+                RegVal = 9999;
+            else if (BrowserVer == 8)
+                RegVal = 8888;
+            else
+                RegVal = 7000;
+
+            // set the actual key
+            string app_name = System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe";
+            if(Registry.CurrentUser.GetValue(app_name) == null)
+            {
+                RegistryKey Key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
+                Key.SetValue(app_name, RegVal, RegistryValueKind.DWord);
+                Key.Close();
+            }
         }
     }
 }
